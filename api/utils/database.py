5aa2e1983074bef6af1de3644278deb3dd423c71
Shriram Sunder <tranquilo12@users.noreply.github.com>
2024-12-27 14:21:46 -0800

    Ok, my execute_python_code works.

--- api/utils/database.py
+++ api/utils/database.py
@@ -2,13 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-from datetime import datetime
 import uuid
 from dotenv import load_dotenv
 
 load_dotenv(".env.local")
 
-DATABASE_URL = os.environ.get("POSTGRES_URL")
+DATABASE_URL = os.environ.get(
+    "POSTGRES_URL",
+    "postgresql://default:zmNuXOkn14TJ@ep-proud-sound-a4rerowu-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -18,3 +20,11 @@ Base = declarative_base()
 
 def generate_uuid():
     return str(uuid.uuid4())
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
